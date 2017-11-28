15f18c54debde7f8fbaad5e24023f65ddaf5680c
Marian Vanderka <vanderka.marian@gmail.com>
2017-11-28 22:39:59 +0100

    link crawler

--- main.py
+++ main.py
@@ -1,9 +1,64 @@
+import argparse
 import os
 from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
 from lxml import html
+from urllib3.util import url
+
+
+class Scraper(object):
+    urls = set()
+    done = set()
+
+    def __init__(self):
+        self.urls = set()
+        self.done = set()
+
+    def get_input(self):
+        parser = argparse.ArgumentParser(description='crawls website tree and downloads images')
+        parser.add_argument('base_url', nargs=1, help="base URL of website to scrape")
+        args = parser.parse_args()
+        self.urls = {args.base_url[0]}
+
+    @staticmethod
+    def one_page_links(page_url):
+        host = url.parse_url(page_url)
+        host_prefix = host.scheme + "://" + host.hostname
+
+        # todo error load page response != 200
+        page = requests.get(page_url)
+        if page.status_code != 200:
+            print("page error %s" % page_url)
+            return set()
+        else:
+            tree = html.fromstring(page.content)
+            links = set(tree.xpath('//a/@href'))
+            urls = [x for x in links if str(x).startswith(host_prefix) and str(x)[-1] == '/']
+            site_urls = [host_prefix + x for x in set(links) if str(x)[0] == '/' and str(x)[-1] == '/']
+            urls.extend(site_urls)
+            return set(urls)
+
+    def get_links(self):
+        if not self.urls:
+            return self.done
+
+        for page in self.urls.copy():
+            print("on page: %s" % page)
+            links = Scraper.one_page_links(page)
+            self.done.add(page)
+            self.urls.remove(page)
+            self.urls |= links - self.done
+
+        # todo there must be a nicer way to write this
+        print("\n")
+        print("urls to crawl:")
+        print(self.urls)
+        print("urls done:")
+        print(self.done)
+        print("\n")
+        return self.get_links()
 
 
 def init_store():
@@ -21,17 +76,6 @@ def one_page_images(url):
     return list(set(images))
 
 
-def one_page_links(url):
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-    links = set(tree.xpath('//a/@href'))
-    urls = [x for x in set(links) if str(x).startswith(main_url)]
-    site = [main_url + x[1:] for x in set(links) if str(x)[0] == '/' and str(x)[-1] == '/']
-    urls.extend(site)
-    print(urls)
-    return list(set(urls))
-
-
 def download_image(img_url):
     img_request = requests.request('get', img_url)
 
@@ -42,9 +86,6 @@ def download_image(img_url):
 
 
 if __name__ == '__main__':
-    main_url = "https://exponea.com/"
-    path = init_store()
-    one_page_links(main_url)
-    # for image in images:
-    #     download_image(image, path)
-    # print(images)
+    scraper = Scraper()
+    scraper.get_input()
+    scraper.get_links()
